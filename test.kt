080708abda9d8279c2507473dde33f0778db51c9
diyora-eng <diyoraorunbaeva@gmail.com>
2026-07-31 13:59:03 +0500

    test

--- test.kt
+++ test.kt
@@ -1,5 +1,16 @@
-
-fun main() {
-    val fin = "fin"
-    println(fin)
+fun splitBill(amount: Double, tipPercent: Int, people: Int): Double {
+val tip = amount * tipPercent / 100
+val total = amount + tip
+val human = total / people
+ return human
 }
+
+fun main(){
+    val human = splitBill(42357.0,10,3)
+    println(" каждый платит:$human")
+    
+}
+
+   
+
+
